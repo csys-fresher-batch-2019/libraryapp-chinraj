a665fafd600fba97f7fd58f2c0c94d1017e220c4
Chinraj M <chinraj044@gmail.com>
2020-02-11 18:39:37 +0530

    updated

--- src/main/java/com/chainsys/LDM/BookList/ExportClassBookList.java
+++ src/main/java/com/chainsys/LDM/BookList/ExportClassBookList.java
@@ -15,9 +15,7 @@ public class ExportClassBookList {
 			String Line = aa.getAuthorName() + "," + aa.getISBN() + "," + aa.getBookName() + "," + aa.getPages() + "," + aa.getPrice() + ","
 					+ aa.getBookStatus() + "," + aa.getRackNo() + "," + aa.getPublication() + "," + aa.getReleasedDate();
 			Filess = Filess + Line + "\n";
-		
 		}
-
 		Path path = Paths.get("D:\\Vignesh.txt");
 		Files.write(path, Filess.getBytes(), StandardOpenOption.APPEND);
 	}
